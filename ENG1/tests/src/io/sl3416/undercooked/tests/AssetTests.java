42866775a94e94c243a61e0e2565b603db270c68
sl3416 <sl3416@york.ac.uk>
2023-03-10 13:13:59 +0000

    Changed path in gradle.yml
    "ENG1/core/build/reports/jacoco/test/html/" to "...testing env/build/reports/jacoco/test/html/"

--- ENG1/tests/src/io/sl3416/undercooked/tests/AssetTests.java
+++ ENG1/tests/src/io/sl3416/undercooked/tests/AssetTests.java
@@ -1,15 +1,10 @@
 package io.sl3416.undercooked.tests;
 
 import com.badlogic.gdx.Gdx;
-import jdk.internal.access.JavaIOFileDescriptorAccess;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.io.File;
-import com.badlogic.gdx.files.FileHandle;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.*;
-
 
 @RunWith(GdxTestRunner.class)
 public class AssetTests {
